79437a7e137061cc9e5a78887ecf972dde266b8d
warwing <warwing@gmx.de>
2016-04-12 22:23:09 +0200

    Added doc for log levels to config

--- config.inc.php
+++ config.inc.php
@@ -24,8 +24,17 @@
     'logger' => array (
         'active' => true,
         'filename' => '/tine20/log/tine20.log',
-        'priority' => '7',
+        'priority' => '5',
     ),
+    // const EMERG   = 0;  // Emergency: system is unusable
+    // const ALERT   = 1;  // Alert: action must be taken immediately
+    // const CRIT    = 2;  // Critical: critical conditions
+    // const ERR     = 3;  // Error: error conditions
+    // const WARN    = 4;  // Warning: warning conditions
+    // const NOTICE  = 5;  // Notice: normal but significant condition
+    // const INFO    = 6;  // Informational: informational messages
+    // const DEBUG   = 7;  // Debug: debug messages
+    // const TRACE   = 8;  // Trace: trace messages (VERY verbose, WARNING: may contain passwords)
     'filesdir'  => '/tine20/files',
     'tmpdir' => '/tine20/tmp',
   );
